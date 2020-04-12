da8449343525f1a7d3cb5cf283caed3f0378667b
Hiram Castillo <hiramcastillo36@gmail.com>
2020-04-12 16:15:10 -0600

    Turtle
    Tortuga con funciones

--- Curso_Python/Turtle.py
+++ Curso_Python/Turtle.py
@@ -1,21 +1,24 @@
 import turtle
 
-window = turtle.Screen()
-tortuguin = turtle.Turtle()
-x = 150
-y = 90 
+def main():
+    window = turtle.Screen()
+    tortuguin = turtle.Turtle()
+    tam=80
+    for i in range(1,15):
+        dibujar(tortuguin, tam)
+        tam = configpos(tortuguin, i, tam)
 
-for i in range(1,15):
-    tortuguin.forward(x)
-    tortuguin.left(y)
-    tortuguin.forward(x)
-    tortuguin.left(y)
-    tortuguin.forward(x)
-    tortuguin.left(y)
-    tortuguin.forward(x)
-    tortuguin.left(y)
-    pos = tortuguin.pos()
-    tortuguin.setpos(i*10,i*10)
-    x=x-20
-    print(pos)
-turtle.mainloop()
+def dibujar(tortuguin, tam):
+    cuadrado(tortuguin, tam)
+
+def configpos(tortuguin, i, tam):
+    tortuguin.setpos(i*5,i*5)
+    return tam-10
+
+def cuadrado(tortuguin, size):
+    for i in range(4):
+        tortuguin.forward(size)
+        tortuguin.left(90)
+
+if __name__ == '__main__':
+    main()
