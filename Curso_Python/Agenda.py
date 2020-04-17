475c642d06319abcfc1ae7d1695ba017dce5f506
Hiram Castillo <hiramcastillo36@gmail.com>
2020-04-16 21:53:28 -0600

    Agenda
    Agenda terminada, datos en csv

--- Curso_Python/Agenda.py
+++ Curso_Python/Agenda.py
@@ -1,20 +1,79 @@
+import csv
 
 class Contacto:
     def __init__(self, name, phone, email):
-        self._name=name
-        self._phone=phone
-        self._email=email
+        self.name=name
+        self.phone=phone
+        self.email=email
 
 class Agenda:
     def __init__(self):
         self._contactos=[]
     
     def add(self, name, phone, email):
-        print("Name: {}, Phone: {}, Email: {}".format(name,phone, email))
+        contacto = Contacto(name, phone, email)
+        self._contactos.append(contacto)
+        self._guardar()
 
+    def mostrar(self):
+        for contacto in self._contactos:
+            self._printcontacto(contacto)
+    
+    def eliminar(self, nombre):
+        for idx, contacto in enumerate(self._contactos):
+            if contacto.name.lower() == nombre.lower():
+                del self._contactos[idx]
+                self._guardar()
+                break
+    
+    def buscar(self, nombre):
+        for idx, contacto in enumerate(self._contactos):
+            if contacto.name.lower() == nombre.lower():
+                self._printcontacto(self._contactos[idx]) #Para funciones privadas solo poner self
+                break
+        else: 
+            self._noencontrado()
+
+    def actualizar(self, nombre, phone, email):
+        for contacto in enumerate(self._contactos):
+            if contacto.name.lower() == nombre.lower():
+                contacto.phone=phone
+                contacto.email=email
+
+    def _printcontacto(self, contacto):
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+        print('Nombre: {}'.format(contacto.name))
+        print('Teléfono: {}'.format(contacto.phone))
+        print('Email: {}'.format(contacto.email))
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+
+    def _noencontrado(self):
+        print('*********')
+        print('NO ENCONTRADO')
+        print('*********')
+
+    def _guardar(self):
+        with open('contactos.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(('name','phone','email'))
+
+            for contacto in self._contactos:
+                writer.writerow((contacto.name, contacto.phone, contacto.email))
 
 def main():
     agenda=Agenda()
+    try:
+        with open('contactos.csv', 'r') as f:
+            reader = csv.reader(f)
+            for idx, row in enumerate(reader):
+                if idx==0:
+                    continue
+                agenda.add(row[0],row[1],row[2])
+    except FileNotFoundError:
+        with open('contactos.csv', 'w', newline='') as f:
+            pass
+
+
     while True:
         command = str(input('''
             ¿Qué deseas hacer?
@@ -34,16 +93,22 @@ def main():
             agenda.add(nombre, telefono, email)
         
         elif command == 'ac':
-            print('actualizar contacto')
-
+            print("A C T U A L I Z A R  C O N T A C T O")
+            nombre = input("Dame el nombre: ")
+            telefono = input("Dame el telefono del contando: ")
+            email = input("Dame el correo: ")
+            agenda.actualizar(nombre, telefono, email)
+            
         elif command == 'b':
-            print('buscar contacto')
+            nombre = input("Dame el nombre para buscar: ")
+            agenda.buscar(nombre)
 
         elif command == 'e':
-            print('eliminar contacto')
+            nombre = input("Dame el nombre para eliminar: ")
+            agenda.eliminar(nombre)
 
         elif command == 'l':
-            print('listar contactos')
+            agenda.mostrar()
 
         elif command == 's':
             break
